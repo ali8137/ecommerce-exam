3b2d325d9cd894846956850648e722d5ffc9025a
ali8137 <124887925+ali8137@users.noreply.github.com>
2025-03-30 13:29:23 +0300

    initialized the backend part of the project, along with setting up the necessary dependencies

---
+++ ecommerce-exam-backend/src/main/java/com/ali/ecommerce_exam_backend/EcommerceExamBackendApplication.java
@@ -0,0 +1,13 @@
+package com.ali.ecommerce_exam_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceExamBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommerceExamBackendApplication.class, args);
+	}
+
+}
